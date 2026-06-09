33671b05be0b6642fc203cde72b8e1036d186353
daniel-vink <danielvink7@gmail.com>
2026-06-09 19:33:13 +0300

    page

--- CardGame/CardGame/ViewController.swift
+++ CardGame/CardGame/ViewController.swift
@@ -1,19 +1,15 @@
-//
-//  ViewController.swift
-//  CardGame
-//
-//  Created by macOS on 09/06/2026.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func btn_name(_ sender: Any) {
+        
+    }
+    
 }
 
